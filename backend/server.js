522ff0114cdf56c1921646b7755afc13d2d7aec4
aaryanmehta26 <aaryanmehta2699@gmail.com>
2024-03-02 12:14:15 +1030

    Initialise backend and frontend

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const notes = require('./data/notes');
+const dotenv = require('dotenv');
+
+const app = express();
+dotenv.config();
+
+app.get('/', (req, res) => {
+    res.send('API running');
+})
+
+app.get('/api/notes', (req, res) => {
+    res.send(notes);
+})
+
+app.get('/api/notes/:id', (req, res) => {
+    const note = notes.find(note => note._id === req.params.id)
+    res.send(note);
+})
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(9000, console.log(`Server started on PORT ${PORT}`));
